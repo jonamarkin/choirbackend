f802e741610b269e65b445f1be3b320e66c5e631
Jonathan Ato Markin <jona10711@gmail.com>
2026-01-03 14:14:11 +0100

    URLS modified

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,21 +1,26 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenRefreshView
+from dj_rest_auth.views import PasswordResetView, PasswordResetConfirmView
 from . import views
 
 router = DefaultRouter(trailing_slash=False)
 router.register('', views.AuthViewSet, basename='auth')
 
 urlpatterns = [
-    # Traditional auth
+
     path('', include(router.urls)),
-
+    
+    # JWT token refresh
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-    # Registration & password reset (from dj-rest-auth)
+    
+    # Registration (from dj-rest-auth)
     path('register/', include('dj_rest_auth.registration.urls')),
-    path('password-reset/', include('dj_rest_auth.urls')),
-
+    
+    # Password reset ONLY
+    path('password/reset/', PasswordResetView.as_view(), name='password_reset'),
+    path('password/reset/confirm/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    
     # Social authentication
     path('social/google/', views.GoogleLogin.as_view(), name='google_login'),
     path('social/github/', views.GitHubLogin.as_view(), name='github_login'),
